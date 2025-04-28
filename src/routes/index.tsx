876e81e96a7f50750a85d86e6bf99964c71ffc1d
Emanuel Claudino <emanuel.teixeira@kxc.com.br>
2025-04-28 15:27:11 -0300

    Feat: deploy

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import { Switch } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes } from './Routes';
 
-import { PrivateRoute } from './PrivateRoute';
-
-import { LoginRoutes } from './login';
-import { Main } from 'pages/Main';
-import { ControlTower } from 'pages/ControlTower';
-import { SettingsRoutes } from './settings';
-import { WmsRoutes } from './wms';
-
-export const Routes: React.FC = () => {
-  return (
-    <>
-      <Switch>
-        <PrivateRoute path="/" exact component={Main} />
-        <PrivateRoute path="/base/base" exact component={Main} />
-        <PrivateRoute path="/control-tower" exact component={ControlTower} />
-      </Switch>
-      <LoginRoutes />
-      <SettingsRoutes />
-      <WmsRoutes/>
-    </>
-  );
-};
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter basename="/base">
+      <Routes />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
